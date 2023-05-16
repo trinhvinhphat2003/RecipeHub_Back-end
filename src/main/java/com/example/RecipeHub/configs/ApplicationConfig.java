eaa8b609cb5159276c71530b202315f8719608a5
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-05-16 16:57:54 +0700

    jwt authen continueing

--- src/main/java/com/example/RecipeHub/configs/ApplicationConfig.java
+++ src/main/java/com/example/RecipeHub/configs/ApplicationConfig.java
@@ -1,8 +1,58 @@
 package com.example.RecipeHub.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.RecipeHub.repositories.UserRepository;
+
 
 @Configuration
 public class ApplicationConfig {
-
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Bean
+	public AuthenticationProvider getAuthenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setUserDetailsService(getUserDetailsService());
+		daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
+		return daoAuthenticationProvider;
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public UserDetailsService getUserDetailsService() {
+		return new UserDetailsService() {
+			
+			@Override
+			public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+				// TODO Auto-generated method stub
+				return userRepository.findByEmail(email)
+						.orElseThrow(() -> {
+							return new UsernameNotFoundException("Account not found");
+						});
+			}
+		};
+	}
+	
+	@Bean
+	public AuthenticationManager getAuthenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+	
 }
