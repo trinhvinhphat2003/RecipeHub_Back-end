1979143eef1319e4686d43663ea33f23195e8f37
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-05-30 15:11:02 +0700

    m

--- src/main/java/com/example/RecipeHub/configs/CorsConfig.java
+++ src/main/java/com/example/RecipeHub/configs/CorsConfig.java
@@ -11,8 +11,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://recipehub-g3.vercel.app")
-                        .allowedOrigins("http://localhost:3000");
+                        .allowedOrigins("http://recipehub-g3.vercel.app");
             }
         };
     }
