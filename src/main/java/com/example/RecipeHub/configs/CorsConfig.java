e0a3c42458fdfb4cf5c7f2d26fe222f0684f26e8
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-05-30 16:42:38 +0700

    m

--- src/main/java/com/example/RecipeHub/configs/CorsConfig.java
+++ src/main/java/com/example/RecipeHub/configs/CorsConfig.java
@@ -5,18 +5,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CorsConfig {
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                		.allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*")
-                        .allowCredentials(true)
-                        .allowedHeaders("*")
-                        .maxAge(3600);
-            }
-        };
-    }
+//	@Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                		.allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedOrigins("*")
+//                        .allowCredentials(true)
+//                        .allowedHeaders("*")
+//                        .maxAge(3600);
+//            }
+//        };
+//    }
 }
