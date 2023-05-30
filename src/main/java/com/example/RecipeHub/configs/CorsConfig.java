dbc5a5b49e7a2d98a92b8ed52eee7b4cf35e7f4a
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-05-30 19:50:23 +0700

    m

--- src/main/java/com/example/RecipeHub/configs/CorsConfig.java
+++ src/main/java/com/example/RecipeHub/configs/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                 		.allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("https://recipehub-g3.vercel.app")
+                        .allowedOrigins("*")
                         .allowCredentials(true)
                         .allowedHeaders("*")
                         .maxAge(3600);
