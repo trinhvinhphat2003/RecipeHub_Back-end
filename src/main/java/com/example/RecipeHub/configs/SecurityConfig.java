0c2e2379052fca60be07527c8756d7aa703892a5
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-06-06 14:22:19 +0700

    test

--- src/main/java/com/example/RecipeHub/configs/SecurityConfig.java
+++ src/main/java/com/example/RecipeHub/configs/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
 				.requestMatchers("/api/v1/admin/**").hasAuthority(Role.ADMIN.name())
 				.requestMatchers("/api/v1/auth/**").permitAll()
 				.requestMatchers("/api/v1/global/**").permitAll()
-				.requestMatchers("/").permitAll()
+				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/api-docs/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
