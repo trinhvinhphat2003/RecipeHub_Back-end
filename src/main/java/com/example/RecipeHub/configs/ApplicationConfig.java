dd241247d9f5f81108eff1eb619ab212d7844bb3
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-06-22 16:19:21 +0700

    new

--- src/main/java/com/example/RecipeHub/configs/ApplicationConfig.java
+++ src/main/java/com/example/RecipeHub/configs/ApplicationConfig.java
@@ -86,150 +86,150 @@ public class ApplicationConfig {
 				
 				
 				
-				// create user
-				userRepository.save(new User("admin@gmail.com", getPasswordEncoder().encode("123456"), Role.ADMIN,
-						"admin", Gender.MALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
-				userRepository.save(new User("user@gmail.com", getPasswordEncoder().encode("123456"), Role.USER, "user",
-						Gender.MALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
-				userRepository.save(new User("user1@gmail.com", getPasswordEncoder().encode("123456"), Role.USER,
-						"user1", Gender.FEMALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
-				userRepository.save(new User("user2@gmail.com", getPasswordEncoder().encode("123456"), Role.USER,
-						"user2", Gender.FEMALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
-
-				// create friend request
-//				friendshipRepository.save(new FriendshipRequest(userRepository.findById(3l).get(),
-//						userRepository.findById(1l).get(), Friendship_status.WAITING));
-//				friendshipRepository.save(new FriendshipRequest(userRepository.findById(2l).get(),
-//						userRepository.findById(1l).get(), Friendship_status.WAITING));
-//				friendshipRepository.save(new FriendshipRequest(userRepository.findById(2l).get(),
-//						userRepository.findById(3l).get(), Friendship_status.ACCEPTED));
-//				friendshipRepository.save(new FriendshipRequest(userRepository.findById(2l).get(),
-//						userRepository.findById(4l).get(), Friendship_status.ACCEPTED));
-
-				// sample add friend
-//				friendService.addFriend(2l, 1l);
-//				friendService.addFriend(2l, 3l);
-//				friendService.addFriend(2l, 4l);
-
-				// create recipe
-				User admin = userRepository.findByEmail("admin@gmail.com").get();
-				User user = userRepository.findByEmail("user@gmail.com").get();
-				
-				tagRepository.save(new Tag("dinner"));
-				tagRepository.save(new Tag("noon"));
-				Tag noon = tagRepository.findByTagName("noon").get();
-				Tag dinner = tagRepository.findByTagName("dinner").get();
-				
-				//1
-				String recipeTag = "dinner";
-				String title = "admin Recipe 1";
-				Integer preTime = 30;
-				Integer cookTime = 60;
-				Integer recipeYield = 4;
-				Integer rating = 5;
-				boolean isFavourite = true;
-				String description = "This recipe 0 is amazing!";
-				String unit = "grams";
-				String steps = "Step 1\nStep 2\nStep 3";
-				String nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
-				PrivacyStatus privacyStatus = PrivacyStatus.PUBLIC;
-
-				Recipe recipe = new Recipe(admin, title, preTime, cookTime, recipeYield, rating, isFavourite,
-						description, unit, steps, nutrition, privacyStatus);
-
-				recipe.getIngredients().add(new Ingredient(null, recipe, "onion", "100 gram"));
-				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
-
-				recipe = recipeRepository.save(recipe);
-				
-				recipe.getTags().add(dinner);
-				
-				recipe = recipeRepository.save(recipe);
-				
-				
-				//2
-				recipeTag = "dinner";
-				title = "admin Recipe 2";
-				preTime = 30;
-				cookTime = 60;
-				recipeYield = 4;
-				rating = 5;
-				isFavourite = true;
-				description = "This recipe 1 is amazing!";
-				unit = "grams";
-				steps = "Step 1\nStep 2\nStep 3";
-				nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
-				privacyStatus = PrivacyStatus.PRIVATE;
-
-				recipe = new Recipe(admin, title, preTime, cookTime, recipeYield, rating, isFavourite, description, unit,
-						steps, nutrition, privacyStatus);
-
-				recipe.getIngredients().add(new Ingredient(null, recipe, "beaf", "1 gram"));
-				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
-
-				recipe = recipeRepository.save(recipe);
-				
-				recipe.getTags().add(dinner);
-				recipe.getTags().add(noon);
-				
-				recipeService.save(recipe);
-				
-				//3
-				recipeTag = "noon";
-				title = "user Recipe 1";
-				preTime = 30;
-				cookTime = 60;
-				recipeYield = 4;
-				rating = 5;
-				isFavourite = true;
-				description = "This recipe 2 is amazing!";
-				unit = "grams";
-				steps = "Step 1\nStep 2\nStep 3";
-				nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
-				privacyStatus = PrivacyStatus.PUBLIC;
-
-				recipe = new Recipe(user, title, preTime, cookTime, recipeYield, rating, isFavourite, description, unit,
-						steps, nutrition, privacyStatus);
-
-				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
-				recipe.getIngredients().add(new Ingredient(null, recipe, "beaf", "1 gram"));
-
-				recipe = recipeRepository.save(recipe);
-				
-				recipe.getTags().add(noon);
-				
-				recipeService.save(recipe);
-				//4
-				recipeTag = "noon";
-				title = "user Recipe 2";
-				preTime = 30;
-				cookTime = 60;
-				recipeYield = 4;
-				rating = 4;
-				isFavourite = true;
-				description = "This recipe 3 is amazing!";
-				unit = "grams";
-				steps = "Step 1\nStep 2\nStep 3";
-				nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
-				privacyStatus = PrivacyStatus.PRIVATE;
-
-				recipe = new Recipe(user, title, preTime, cookTime, recipeYield, rating, isFavourite, description, unit,
-						steps, nutrition, privacyStatus);
-
-				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
-				recipe.getIngredients().add(new Ingredient(null, recipe, "beaf", "1 gram"));
-				recipe.getIngredients().add(new Ingredient(null, recipe, "onion", "1 gram"));
-
-				recipe = recipeRepository.save(recipe);
-				
-				recipe.getTags().add(noon);
-				recipe.getImages().add(new Image(null, "this is url for image", recipe));
-				
-				recipe = recipeRepository.save(recipe);
-				recipeTag = "breakfast";
-				Tag breakfast = tagRepository.save(new Tag("breakfast"));
-				recipe.getTags().add(breakfast);
-				recipeService.save(recipe);
+//				// create user
+//				userRepository.save(new User("admin@gmail.com", getPasswordEncoder().encode("123456"), Role.ADMIN,
+//						"admin", Gender.MALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
+//				userRepository.save(new User("user@gmail.com", getPasswordEncoder().encode("123456"), Role.USER, "user",
+//						Gender.MALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
+//				userRepository.save(new User("user1@gmail.com", getPasswordEncoder().encode("123456"), Role.USER,
+//						"user1", Gender.FEMALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
+//				userRepository.save(new User("user2@gmail.com", getPasswordEncoder().encode("123456"), Role.USER,
+//						"user2", Gender.FEMALE, true, DateTimeUtil.milisecondToDate(System.currentTimeMillis()), avartarBaseUrl + "default.jpg", LoginType.BASIC));
+//
+//				// create friend request
+////				friendshipRepository.save(new FriendshipRequest(userRepository.findById(3l).get(),
+////						userRepository.findById(1l).get(), Friendship_status.WAITING));
+////				friendshipRepository.save(new FriendshipRequest(userRepository.findById(2l).get(),
+////						userRepository.findById(1l).get(), Friendship_status.WAITING));
+////				friendshipRepository.save(new FriendshipRequest(userRepository.findById(2l).get(),
+////						userRepository.findById(3l).get(), Friendship_status.ACCEPTED));
+////				friendshipRepository.save(new FriendshipRequest(userRepository.findById(2l).get(),
+////						userRepository.findById(4l).get(), Friendship_status.ACCEPTED));
+//
+//				// sample add friend
+////				friendService.addFriend(2l, 1l);
+////				friendService.addFriend(2l, 3l);
+////				friendService.addFriend(2l, 4l);
+//
+//				// create recipe
+//				User admin = userRepository.findByEmail("admin@gmail.com").get();
+//				User user = userRepository.findByEmail("user@gmail.com").get();
+//				
+//				tagRepository.save(new Tag("dinner"));
+//				tagRepository.save(new Tag("noon"));
+//				Tag noon = tagRepository.findByTagName("noon").get();
+//				Tag dinner = tagRepository.findByTagName("dinner").get();
+//				
+//				//1
+//				String recipeTag = "dinner";
+//				String title = "admin Recipe 1";
+//				Integer preTime = 30;
+//				Integer cookTime = 60;
+//				Integer recipeYield = 4;
+//				Integer rating = 5;
+//				boolean isFavourite = true;
+//				String description = "This recipe 0 is amazing!";
+//				String unit = "grams";
+//				String steps = "Step 1\nStep 2\nStep 3";
+//				String nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
+//				PrivacyStatus privacyStatus = PrivacyStatus.PUBLIC;
+//
+//				Recipe recipe = new Recipe(admin, title, preTime, cookTime, recipeYield, rating, isFavourite,
+//						description, unit, steps, nutrition, privacyStatus);
+//
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "onion", "100 gram"));
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
+//
+//				recipe = recipeRepository.save(recipe);
+//				
+//				recipe.getTags().add(dinner);
+//				
+//				recipe = recipeRepository.save(recipe);
+//				
+//				
+//				//2
+//				recipeTag = "dinner";
+//				title = "admin Recipe 2";
+//				preTime = 30;
+//				cookTime = 60;
+//				recipeYield = 4;
+//				rating = 5;
+//				isFavourite = true;
+//				description = "This recipe 1 is amazing!";
+//				unit = "grams";
+//				steps = "Step 1\nStep 2\nStep 3";
+//				nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
+//				privacyStatus = PrivacyStatus.PRIVATE;
+//
+//				recipe = new Recipe(admin, title, preTime, cookTime, recipeYield, rating, isFavourite, description, unit,
+//						steps, nutrition, privacyStatus);
+//
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "beaf", "1 gram"));
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
+//
+//				recipe = recipeRepository.save(recipe);
+//				
+//				recipe.getTags().add(dinner);
+//				recipe.getTags().add(noon);
+//				
+//				recipeService.save(recipe);
+//				
+//				//3
+//				recipeTag = "noon";
+//				title = "user Recipe 1";
+//				preTime = 30;
+//				cookTime = 60;
+//				recipeYield = 4;
+//				rating = 5;
+//				isFavourite = true;
+//				description = "This recipe 2 is amazing!";
+//				unit = "grams";
+//				steps = "Step 1\nStep 2\nStep 3";
+//				nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
+//				privacyStatus = PrivacyStatus.PUBLIC;
+//
+//				recipe = new Recipe(user, title, preTime, cookTime, recipeYield, rating, isFavourite, description, unit,
+//						steps, nutrition, privacyStatus);
+//
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "beaf", "1 gram"));
+//
+//				recipe = recipeRepository.save(recipe);
+//				
+//				recipe.getTags().add(noon);
+//				
+//				recipeService.save(recipe);
+//				//4
+//				recipeTag = "noon";
+//				title = "user Recipe 2";
+//				preTime = 30;
+//				cookTime = 60;
+//				recipeYield = 4;
+//				rating = 4;
+//				isFavourite = true;
+//				description = "This recipe 3 is amazing!";
+//				unit = "grams";
+//				steps = "Step 1\nStep 2\nStep 3";
+//				nutrition = "Protein: 10g\nCarbohydrates: 20g\nFat: 5g";
+//				privacyStatus = PrivacyStatus.PRIVATE;
+//
+//				recipe = new Recipe(user, title, preTime, cookTime, recipeYield, rating, isFavourite, description, unit,
+//						steps, nutrition, privacyStatus);
+//
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "carrot", "1 gram"));
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "beaf", "1 gram"));
+//				recipe.getIngredients().add(new Ingredient(null, recipe, "onion", "1 gram"));
+//
+//				recipe = recipeRepository.save(recipe);
+//				
+//				recipe.getTags().add(noon);
+//				recipe.getImages().add(new Image(null, "this is url for image", recipe));
+//				
+//				recipe = recipeRepository.save(recipe);
+//				recipeTag = "breakfast";
+//				Tag breakfast = tagRepository.save(new Tag("breakfast"));
+//				recipe.getTags().add(breakfast);
+//				recipeService.save(recipe);
 
 			}
 		};
