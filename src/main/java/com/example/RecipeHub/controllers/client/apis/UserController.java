d0fef8b3c4585650fbe8ede16e6ec9e94d8685f8
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-07-04 13:36:05 +0700

    complete some api

--- src/main/java/com/example/RecipeHub/controllers/client/apis/UserController.java
+++ src/main/java/com/example/RecipeHub/controllers/client/apis/UserController.java
@@ -2,12 +2,15 @@ package com.example.RecipeHub.controllers.client.apis;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.RecipeHub.client.dtos.EditProfileRequest;
+import com.example.RecipeHub.client.dtos.UserDTO;
 import com.example.RecipeHub.entities.User;
+import com.example.RecipeHub.mappers.UserMapper;
 import com.example.RecipeHub.services.UserService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -27,5 +30,11 @@ public class UserController {
 	public ResponseEntity<String> editProfile(@AuthenticationPrincipal User user, @RequestBody EditProfileRequest request) {
 		userService.editProfile(request, user.getUserId());
 		return ResponseEntity.ok("profile is edited successfully");
+	}
+	
+	@GetMapping("/user/profile")
+	public ResponseEntity<UserDTO> getUser(@AuthenticationPrincipal User user) {
+		UserDTO result = UserMapper.INSTANCE.userToUserDTO(userService.getUserById(user.getUserId()));
+		return ResponseEntity.ok(result);
 	}
 }
