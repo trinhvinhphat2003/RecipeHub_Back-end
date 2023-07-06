dfc426faaa94a2f0425a1666588cf842f9882899
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-07-06 20:42:59 +0700

    complete some api

--- src/main/java/com/example/RecipeHub/controllers/client/apis/UserController.java
+++ src/main/java/com/example/RecipeHub/controllers/client/apis/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,6 @@ import com.example.RecipeHub.entities.User;
 import com.example.RecipeHub.mappers.UserMapper;
 import com.example.RecipeHub.services.UserService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1")
