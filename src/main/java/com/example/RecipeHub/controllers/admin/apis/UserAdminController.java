4c42eb85d5713bfe1f3e46c3ca0a05182709ad22
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-07-28 09:45:02 +0700

    dashboard api

--- src/main/java/com/example/RecipeHub/controllers/admin/apis/UserAdminController.java
+++ src/main/java/com/example/RecipeHub/controllers/admin/apis/UserAdminController.java
@@ -34,7 +34,7 @@ public class UserAdminController {
 			@RequestParam(value = "page", defaultValue = "0", required = false) int page,
 			@RequestParam(value = "size", defaultValue = "20", required = false) int size,
 			@RequestParam(value = "query", defaultValue = "", required = false) String query,
-			@RequestParam(value = "isBlocked", required = false, defaultValue = "0") Integer isBlocked,
+			@RequestParam(value = "isBlocked", required = false) Integer isBlocked,
 			@RequestParam(value = "sort", defaultValue = "user_id", required = false) String sort,
 			@RequestParam(value = "direction", defaultValue = "desc", required = false) String direction) { 
 		UsersPaginationResponse response = new UsersPaginationResponse(userService.filterUserAndPagination(page, size, sort, direction, query, isBlocked), userService.countOfFilterUser(sort, direction, query, isBlocked));
