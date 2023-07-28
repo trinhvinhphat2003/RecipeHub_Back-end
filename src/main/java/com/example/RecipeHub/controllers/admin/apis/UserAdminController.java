a06ca9f9aa45975686570a64ef341223dde60421
trinhvinhphat2003 <trinhvinhphat2003@gmail.com>
2023-07-28 10:26:44 +0700

    dashboard api

--- src/main/java/com/example/RecipeHub/controllers/admin/apis/UserAdminController.java
+++ src/main/java/com/example/RecipeHub/controllers/admin/apis/UserAdminController.java
@@ -37,6 +37,7 @@ public class UserAdminController {
 			@RequestParam(value = "isBlocked", required = false, defaultValue = "2") Integer isBlocked,
 			@RequestParam(value = "sort", defaultValue = "user_id", required = false) String sort,
 			@RequestParam(value = "direction", defaultValue = "desc", required = false) String direction) { 
+		if(isBlocked.equals(2)) isBlocked = null;
 		UsersPaginationResponse response = new UsersPaginationResponse(userService.filterUserAndPagination(page, size, sort, direction, query, isBlocked), userService.countOfFilterUser(sort, direction, query, isBlocked));
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
